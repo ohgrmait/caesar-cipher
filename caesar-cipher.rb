4dd7e01b5874c7f861347e5267f7835f00a938d8
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-16 09:45:30 +0530

    Refactor code as per new learning in two lessons
    These are the 'Project Management' and 'Linting and RuboCop' lessons.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,7 @@
+# rubocop:disable Naming/FileName,Lint/MissingCopEnableDirective
+
+# frozen_string_literal: true
+
 def downcase?(char)
   char.ord >= 97 && char.ord <= 122
 end
@@ -6,16 +10,16 @@ def upcase?(char)
   char.ord >= 65 && char.ord <= 90
 end
 
-def caesar_cipher(string, shift_factor)
+def caesar_cipher(string, shift_factor) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
   cipher_text = ''
   string.each_char do |char|
-    if upcase?(char)
-      cipher_text += (((char.ord + shift_factor) - 'A'.ord) % 26 + 'A'.ord).chr
-    elsif downcase?(char)
-      cipher_text += (((char.ord + shift_factor) - 'a'.ord) % 26 + 'a'.ord).chr
-    else
-      cipher_text += char
-    end
+    cipher_text += if upcase?(char)
+                     ((((char.ord + shift_factor) - 'A'.ord) % 26) + 'A'.ord).chr
+                   elsif downcase?(char)
+                     ((((char.ord + shift_factor) - 'a'.ord) % 26) + 'a'.ord).chr
+                   else
+                     char
+                   end
   end
   cipher_text
 end
